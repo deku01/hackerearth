e7d8be21526e69ddb185128d718587784a02de50
Dharmendra Kushwaha <deku37@gmail.com>
2019-08-22 03:24:48 +0530

    Add files via upload

---
+++ Basic Programming/Basics of InputOutput/Ladderophilia.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Ladderophilia{
+    public static void main(String args[] ) throws Exception {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println("*   *");
+        System.out.println("*   *");
+        for(int i=0;i<n;i++){
+            System.out.println("*****");
+            System.out.println("*   *");
+            System.out.println("*   *");
+        }
+    }
+}
