8bce6522bdae2d7188a1f812c06ccc5b68a4c5f6
Dharmendra Kushwaha <deku37@gmail.com>
2019-09-07 23:43:29 +0530

    Update Sum.java

--- Basic Programming/Basics of InputOutput/Sum.java
+++ Basic Programming/Basics of InputOutput/Sum.java
@@ -1,3 +1,22 @@
+/*
+Problem Description:
+Given a series of integer pairs  and , output the sum of  and .
+
+Input Format:
+Each line contains two integers,  and . One input file may contain several pairs  where .
+
+Output Format:
+Output a single integer per line - The sum of  and .
+
+Constraints:
+0<=A,B<=10^98
+
+SAMPLE INPUT                            SAMPLE OUTPUT 
+1 2                                           3   
+2 5                                           7      
+10 14                                         24  
+*/
+
 import java.util.Scanner;
 import java.math.*;
 class Sum {
